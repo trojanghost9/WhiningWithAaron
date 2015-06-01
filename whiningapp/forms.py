0e66ac13630e0fb11d471aed8f3b74a81a9b1b91
ABean06 <aaron.bean06@gmail.com>
2015-06-01 14:19:20 -0700

    push whining

---
+++ whiningapp/forms.py
@@ -0,0 +1,38 @@
+from flask_wtf import Form
+from wtforms import StringField, TextAreaField, SubmitField, BooleanField, validators, SelectField
+
+class wineform(Form):
+    # List of wine color, used for dropdown menu - red or white
+    wine_color = [(""), ("Red"), ("White")]
+
+    # List of wine sweetness, used for dropdown menu - sweet, semi-dry, or dry
+    wine_sweetness = [(""), ("Sweet"), ("Semi-dry"), ("Dry")]
+
+    # Rank how the wine tasted, used for dropdown menu
+    #May give error if don't have ie ("Ehh","Ehh") which is ("submitted", "displayed")
+    wine_taste = [(""), ("Hated It!"), ("Didn't like it"), ("Ehh"), ("Okay"), ("Good"),\
+                  ("Really Good"), ("I'll take a case")]
+
+    #Type of wine tasted.  Example Pinot
+    wine_type = StringField("Type (Example: Pinot) *", [validators.DataRequired("Please enter the wine type.")])
+
+    #Name of wine tasted.
+    wine_name = StringField("Name *", [validators.DataRequired("Please enter the name of the wine.")])
+
+    #Vintage of wine tasted.
+    wine_vintage = StringField("Vintage *", [validators.DataRequired("Please enter the vintage of the wine.")])
+
+    #Country of wine tasted.
+    wine_country = StringField("Country *", [validators.DataRequired("Please enter the wine's country of origin.")])
+
+    #State of wine tasted.
+    wine_state = StringField("State *", [validators.DataRequired("Please enter the wine's state of origin.")])
+
+    #Vineyard of wine tasted.
+    wine_vineyard = StringField("Vineyard *", [validators.DataRequired("Please enter the wine's vineyard  of origin.")])
+
+    #Comments section.  Expandable text field to enter any comments.  Not required.
+    wine_comments = TextAreaField("Any comments or additional notes?")
+
+    #Submit form.
+    submit = SubmitField("Submit My Review")
